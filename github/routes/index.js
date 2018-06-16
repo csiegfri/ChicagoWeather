af2efd2dfec507c3df1e4cdb0d8c18128309b368
root <root@localhost.localdomain>
2018-06-16 13:14:21 -0500

    Grabbed Week 3 code

--- github/routes/index.js
+++ github/routes/index.js
@@ -2,8 +2,54 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// Re-grabbing the code from Week 2 and Week 1
+const requ = require('request-promise-native');
+
+const options = {
+  uri: 'https://api.github.com/users/csiegfri',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
+
+};
+
+var jlogin;
+var jid;
+var javatar;
+var jhtml_url;
+var jname;
+var jbio;
+var jupdated_at;
+
+router.get('/', function(req, res, next){
+  requ(options)
+    .then(function(profile_json){
+      jlogin = JSON.stringify(profile_json.login);
+      jid = JSON.stringify(profile_json.id);
+      javatar = JSON.stringify(profile_json.avatar_url);
+      jhtml_url = JSON.stringify(profile_json.html_url);
+      jname = JSON.stringify(profile_json.name);
+      jbio = JSON.stringify(profile_json.bio);
+      jupdated_at = JSON.stringify(profile_json.updated_at);
+  });
+  console.log(jlogin);
+  next();
+});
+
+
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Index',
+    log: jlogin,
+    id: jid,
+    ava: javatar,
+    html: jhtml_url,
+    name: jname,
+    bio: jbio,
+    update: jupdated_at });
 });
 
 module.exports = router;
