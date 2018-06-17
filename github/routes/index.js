c5a2df99a1f6b86d77e04a60eead13dedf003451
root <root@localhost.localdomain>
2018-06-16 19:00:00 -0500

    testing the create and append method

--- github/routes/index.js
+++ github/routes/index.js
@@ -77,45 +77,71 @@ function direction(degrees){
 }
 
 function weatherReport(){
-  var template = document.getElementById('report');
-  var reports = document.getElementById('allReports');
-  var templateArea = document.querySelector("#report");
-  var reports = document.querySelector("#allReports");
-
-  var clone = document.importNode(templateArea.content,true);
+  var table = document.getElementById("allReports");
 
   for(let x = 0; x < obj.list.length; x++){
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+  //  var txt;
+    td.appendChild();
     cTemp = (obj.list[x].main.temp * (9/5)) - 459.67; // converts k to F
     minTemp = (obj.list[x].main.temp_min * (9/5)) - 459.67;
     maxTemp = (obj.list[x].main.temp_max * (9/5)) - 459.67;
     windDirection = direction(obj.list[x].wind.deg); // converts degree to cardinal direction
     //adjusting values of the fields
-    var nDate = clone.querySelector("#date");
-    nDate.textContent = obj.list[x].date_txt;
-    var nTemp = clone.querySelector("#temp");
-    nTemp.textContent = cTemp;
-    var nMin = clone.querySelector("#min_temp");
-    nMin.textContent = minTemp;
-    var nMax = clone.querySelector("#max_temp");
-    nMax.textContent = maxTemp;
-    var nWeath = clone.querySelector("#weather");
-    nWeath.textContent = obj.list[x].weather[0].main;
-    var nDesc = clone.querySelector("#weather_desc");
-    nDesc.textContent = obj.list[x].weather[0].description;
-    var nHum = clone.querySelector("#humidity");
-    nHum.textContent = obj.list[x].main.humidity;
-    var nSpeed = clone.querySelector("#wind_speed");
-    nSpeed.textContent = obj.list[x].wind.speed;
-    var nDeg = clone.querySelector("#wind_degree");
-    nDeg.textContent = windDirection;
-    var nPres = clone.querySelector("#pressure");
-    nPres.textContent = obj.list[x].main.pressure;
-    var nSea = clone.querySelector("#sea_level");
-    nSea.textContent = obj.list[x].main.sea_level;
-    var nGrn = clone.querySelector("#ground_level");
-    nGrn.textContent = obj.list[x].main.grnd_level;
-    var nCloud = clone.querySelector("#cloudiness");
-    nCloud.textContent = obj.list[x].clouds.all;
+
+    var nDate = obj.list[x].dt_txt;
+    td.innerHtml = JSON.stringify(nDate);
+  //  txt = document.createTextNode(JSON.stringify(nDate));
+    //td.appendChild(txt);
+    tr.appendChild(td);
+
+    var nTemp = cTemp;
+    td.innerHtml = nTemp;
+    tr.appendChild(td);
+
+    var nMin = minTemp;
+    td.innerHtml = nMin;
+    tr.appendChild(td);
+
+    var nMax = maxTemp;
+    td.innerHtml = nMax;
+    tr.appendChild(td);
+
+    var nWeath = obj.list[x].weather[0].main;
+    td.innerHtml = JSON.stringify(nWeath);
+    tr.appendChild(td);
+
+    var nDesc = obj.list[x].weather[0].description;
+    td.innerHtml = JSON.stringify(nDesc);
+    tr.appendChild(td);
+
+    var nHum = obj.list[x].main.humidity;
+    td.innerHtml = JSON.stringify(nHum);
+    tr.appendChild(td);
+
+    var nSpeed = obj.list[x].wind.speed;
+    td.innerHtml = JSON.stringify(nSpeed);
+    tr.appendChild(td);
+
+    td.innerHtml = windDirection;
+    tr.appendChild(td);
+
+    var nPres = obj.list[x].main.pressure;
+    td.innerHtml = JSON.stringify(nPres);
+    tr.appendChild(td);
+
+    var nSea = obj.list[x].main.sea_level;
+    td.innerHtml = JSON.stringify(nSea);
+    tr.appendChild(td);
+
+    var nGrn = obj.list[x].main.grnd_level;
+    td.innerHtml = JSON.stringify(nGrn);
+    tr.appendChild(td);
+
+    var nCloud = obj.list[x].clouds.all;
+    td.innerHtml = nCloud;
+    tr.appendChild(td);
 
     //appending the template to the table
     reports.appendChild(clone);
