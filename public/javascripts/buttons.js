a22b68859ae4207a5de853667dc45e8c0dc73cec
root <root@localhost.localdomain>
2018-06-30 14:47:38 -0500

    added api call to button, variable to hold it all

--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -1,6 +1,14 @@
 const requ = require('request-promise-native');
 
 let options = {};
+//Contains all conversions from original values.
+let conversions = {
+  mintemp:0,
+  cTemp:0,
+  maxTemp:0,
+  windDirection:"Undefined",
+  windSpeed:0
+};
 
 let app = angular.module('App',[]);
 app.controller('Controller',function($scope){
@@ -24,10 +32,41 @@ app.controller('Controller',function($scope){
       },
       json: true
     };
-    
+
     requ(options)
       .then(function(weather_json)){
+        let obj = weather_json;
+        let fullReport = [];
 
-      }
+        for(let x = 0; x < obj.list.length; x++){
+
+          conversions.cTemp = obj.list[x].main.temp ; // Already in correct format
+          conversions.minTemp = obj.list[x].main.temp_min ;
+          conversions.maxTemp = obj.list[x].main.temp_max ;
+          conversions.windDirection = direction(obj.list[x].wind.deg); // converts degree notation to cardinal direction
+          conversions.windSpeed = obj.list[x].wind.speed * 2.23694; // converts m/s to mph
+          //adjusting values of the fields
+          let report = {
+            date: obj.list[x].dt_txt,
+            temp: conversions.cTemp.toFixed(1),
+            min: conversions.minTemp.toFixed(1),
+            max: conversions.maxTemp.toFixed(1),
+            weath: obj.list[x].weather[0].main,
+            hum: obj.list[x].main.humidity,
+            speed: conversions.windSpeed.toFixed(2),
+            dir: conversions.windDirection,
+            pres: obj.list[x].main.pressure,
+            cloud: obj.list[x].clouds.all
+          }
+          //date time in yyyy-mm-dd hh:mm:ss
+          //Wind default m/s
+          //wind direction default uses degree notation
+          //humidity %, cloudiness is % of cloud cover
+          fullReport.push(report);
+        };
+        let table = document.getElementById('generatedReports');
+        
+      };
+
   }
 })
