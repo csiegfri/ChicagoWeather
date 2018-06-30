611e9e57086907fb53d4c8114cfbf938954afed9
root <root@localhost.localdomain>
2018-06-30 12:26:21 -0500

    added dropdown targets

---
+++ public/javascripts/buttons.js
@@ -0,0 +1,6 @@
+let app = angular.module('App',[]);
+app.controller('Controller',function($scope){
+  $scope.cities = ["Chicago","Aurora","Springfield","Rockford","Peoria"];
+  $scope.measures = ["Imperial","Metric"];
+  $scope.display = [5,10,15,"All"];
+})
