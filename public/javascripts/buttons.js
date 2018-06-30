184167be75ef8fb1776eccf94d5b4d0018b54015
root <root@localhost.localdomain>
2018-06-30 12:51:09 -0500

    Added function for button and id to tbody

--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -3,4 +3,8 @@ app.controller('Controller',function($scope){
   $scope.cities = ["Chicago","Aurora","Springfield","Rockford","Peoria"];
   $scope.measures = ["Imperial","Metric"];
   $scope.display = [5,10,15,"All"];
+
+  $scope.drawTable = function(city, unit, disp){
+    
+  }
 })
