972afdefff69880e75c2f013f3210ac7d4b5c480
root <root@localhost.localdomain>
2018-06-30 13:51:17 -0500

    added a customized call in the drawTable function. adjusts api url depending on user input

--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -1,10 +1,33 @@
+const requ = require('request-promise-native');
+
+let options = {};
+
 let app = angular.module('App',[]);
 app.controller('Controller',function($scope){
   $scope.cities = ["Chicago","Aurora","Springfield","Rockford","Peoria"];
-  $scope.measures = ["Imperial","Metric"];
+  let ids = ["4887398","4883817","4896861","4907959","4905687"];
+  $scope.measures = ["imperial","metric"];
   $scope.display = [5,10,15,"All"];
 
   $scope.drawTable = function(city, unit, disp){
+    let id = "";
+    for(let x = 0; x < $scope.cities.length; x++){
+      if(city === $scope.cities[x]){
+        id = ids[x];
+      }
+    };
+    let url = "http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&appid=083149282228ade2d95a8d79cd581982&units=" + unit;
+    options = {
+      uri: url,
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    };
     
+    requ(options)
+      .then(function(weather_json)){
+
+      }
   }
 })
